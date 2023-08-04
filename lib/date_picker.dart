47811293c489994fe666eff61d808da40a45cf60
qiaodongliang <qiaodongliang01@163.com>
2023-08-04 16:17:37 +0800

    Update and rename scroll_date_picker.dart to date_picker.dart

--- lib/date_picker.dart
+++ lib/date_picker.dart
@@ -1,4 +1,5 @@
-export 'src/scroll_date_picker.dart';
+export 'src/date_picker.dart';
 export 'src/constants.dart';
 export 'src/models/date_picker_options.dart';
 export 'src/models/date_picker_scroll_view_options.dart';
+
